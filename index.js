8dc56aeaa00415da4dc734a923f99cb1810a4d2a
Vas Leckas <vas.leckas@gmail.com>
2015-08-25 17:28:57 -0500

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('Express')
 var path = require('path');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
+//app.use(express.static(path.join(__dirname, './node_modules')));
+
 
 var server = app.listen(3000, function(){
     var port = server.address().port;
