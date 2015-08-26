600147bbe492aa0b89af5688d6bf3b035b05ee71
Vas Leckas <vas.leckas@gmail.com>
2015-08-26 08:28:43 -0500

    updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
-//app.use(express.static(path.join(__dirname, './node_modules')));
+app.use('/scripts', express.static(path.join(__dirname, './node_modules/bootstrap/dist/')));
 
 
 var server = app.listen(process.env.PORT || 3000, function(){
